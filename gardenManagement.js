5667e1563f2f4170324f3d21bd2d7b34bd911fe3
NiaBytes <180058599+NiaBytes@users.noreply.github.com>
2026-08-07 17:58:37 -0500

    I used some prior knowledge of functions to make this work, I hope that's okay

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,31 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+function wateringControl() {
+  if (temperature > 80) {
+    console.log("Watering on");
+  } else {
+    console.log("Watering off");
+  }
+}
+
+  function gardenLightsControl() {
+    if (timeOfDay == "evening" || timeOfDay == "night") {
+      console.log("Lights on");
+    } else {
+      console.log("Lights off");
+    }
+  }
+
+  function soilMoistureAdjustment() {
+    while (soilMoisture < 40) {
+        soilMoisture += 5
+        console.log(soilMoisture)
+    }
+  }
+
+
+wateringControl()
+gardenLightsControl()
+soilMoistureAdjustment()
